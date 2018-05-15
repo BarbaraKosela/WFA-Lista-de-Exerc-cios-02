43f0df61ae11ec6a885baaf9e747bd2712b2e713
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-05-15 14:41:11 -0300

    Adição de exercício 02;
    Neste exercício vemos se o número que o usuário escolheu é negativo ou não, utilizando if (adição de try & catch);

--- ListaDeExercicios02/ListaDeExercicios02/Exercicio02.Designer.cs
+++ ListaDeExercicios02/ListaDeExercicios02/Exercicio02.Designer.cs
@@ -28,20 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TXTNUMERO = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Square721 BT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(319, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DIGITE UM NÚMERO DE SUA ESCOLHA:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Square721 BT", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(28, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(263, 57);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "DESCUBRA SE SEU NÚMERO É NEGATIVO OU NÃO";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TXTNUMERO
+            // 
+            this.TXTNUMERO.Location = new System.Drawing.Point(28, 33);
+            this.TXTNUMERO.Name = "TXTNUMERO";
+            this.TXTNUMERO.Size = new System.Drawing.Size(263, 20);
+            this.TXTNUMERO.TabIndex = 2;
             // 
             // Exercicio02
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(330, 133);
+            this.Controls.Add(this.TXTNUMERO);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Name = "Exercicio02";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Exercicio02";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TXTNUMERO;
     }
 }
--- ListaDeExercicios02/ListaDeExercicios02/Exercicio02.cs
+++ ListaDeExercicios02/ListaDeExercicios02/Exercicio02.cs
@@ -12,9 +12,40 @@ namespace ListaDeExercicios02
 {
     public partial class Exercicio02 : Form
     {
+        int numero = 0;
         public Exercicio02()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArmazenarNumero();
+            if (numero < 0)
+            {
+                MessageBox.Show("Parabéns, seu número é negativo!");
+            }
+
+            else if (numero > 0)
+            {
+                MessageBox.Show("Não, seu número não é negativo :C");
+            }
+        }
+
+        private void ArmazenarNumero()
+        {
+            try
+            {
+            numero = Convert.ToInt32(TXTNUMERO.Text);
+
+            }
+            catch
+            {
+                MessageBox.Show("Você pode apenas digitar números reais neste campo, tente novamente");
+                TXTNUMERO.Focus();
+                return; 
+            }
+
         }
     }
 }
